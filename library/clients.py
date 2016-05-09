dbc32adbf143093c50546ea9ca6e69befafafc54
James Denton <james.denton@rackspace.com>
2016-05-09 03:52:44 -0500

    Support for v2 and v3 Keystone

--- library/clients.py
+++ library/clients.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
 # Boilerplate for preparing client objects
-import os, sys
+import os, sys, logging
+from urlparse import urlparse
+from keystoneclient import session, exceptions
+from keystoneclient.exceptions import AuthorizationFailure, Unauthorized
 
 # v2 client
-from keystoneclient.v2_0 import client as kclient
+from keystoneclient.auth.identity import v2
+from keystoneclient.v2_0 import client as k2client
+
+# v3 client
+from keystoneclient.auth.identity import v3
+from keystoneclient.v3 import client as k3client
+
+# Neutron client
 from neutronclient.v2_0 import client as nclient
 
 _keystone_creds = {}
@@ -16,5 +26,30 @@ except KeyError, e:
     print ("Openstack environment variable %s is not set!") % e
     sys.exit(1)
 
-keystone = kclient.Client(**_keystone_creds)
+# Evaluate the auth URL to determine if user is using v2 or v3 Keystone API
+u = urlparse(_keystone_creds['auth_url'])
+
+if "v2.0" in u.path:
+    try:
+	auth = v2.Password(**_keystone_creds)
+	sess = session.Session(auth=auth)
+	keystone = k2client.Client(session=sess)
+    except AuthorizationFailure as auf:
+        print(auf.message)
+    except Unauthorized as unauth:
+        print(unauth.message)
+elif "v3" in u.path:
+    try:
+	auth = v3.Password(**_keystone_creds)
+	sess = session.Session(auth=auth)
+	keystone = k3client.Client(session=sess)
+    except AuthorizationFailure as auf:
+        print(auf.message)
+    except Unauthorized as unauth:
+       	print(unauth.message)
+else:
+    print ("Error: Invalid authentication URL set!")
+
+
+# Instantiate Neutron client
 neutron = nclient.Client(**_keystone_creds)
