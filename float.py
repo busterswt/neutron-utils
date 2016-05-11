eeb8d504eef022bc4b56526b0fb51945c33fd359
James Denton <james.denton@rackspace.com>
2016-05-11 18:37:55 +0000

    Added column filter

--- float.py
+++ float.py
@@ -1,5 +1,5 @@
 import requests, json, sys
-import argparse
+import argparse, string
 from netaddr import *
 from prettytable import PrettyTable
 from library.clients import neutron
@@ -40,21 +40,21 @@ def get_agent(router_id):
 def build_float_table(args):
 
     # Get a list of external networks
-    if args.floatingip is not None:
-	_params = {'floating_ip_address':args.floatingip}
+    if args.floating_ip is not None:
+	_params = {'floating_ip_address':args.floating_ip}
         floatingips = neutron.list_floatingips(**_params)
     elif args.network_id is not None:
         _params = {'floating_network_id':args.network_id}
         floatingips = neutron.list_floatingips(**_params)
-    elif args.fixedip is not None:
-        _params = {'fixed_ip_address':args.fixedip}
+    elif args.fixed_ip is not None:
+        _params = {'fixed_ip_address':args.fixed_ip}
         floatingips = neutron.list_floatingips(**_params)
     elif args.tenant_id is not None:
         _params = {'tenant_id':args.tenant_id}
         floatingips = neutron.list_floatingips(**_params)
 
     # Using returned networks, build a table of subnets and floating info
-    table = PrettyTable(["Floating Network", "Floating IP", "Fixed IP", "Fixed Network", "Router ID", "Agent", "Instance ID"])
+    table = PrettyTable(["tenant_id","floating_network_name", "floating_ip_address", "fixed_ip_address", "fixed_network_id", "router_id", "agent_name", "instance_id"])
     for floatingip in floatingips['floatingips']:
 
 	instanceid = None
@@ -69,22 +69,30 @@ def build_float_table(args):
             agent_name = get_agent(floatingip['router_id'])
 
 	# Print network as the first line
-	table.add_row([get_network_name(floatingip['floating_network_id']),
+	table.add_row([floatingip['tenant_id'],get_network_name(floatingip['floating_network_id']),
 			floatingip['floating_ip_address'],floatingip['fixed_ip_address'],get_network_id(floatingip['port_id']),
 			floatingip['router_id'],agent_name,instanceid])
 
 
-    # Print the table
+    # Print the table (Limit columns returned)
+    if args.fields is not None:
+	fields = args.fields.split(",")
+	try:
+      	    table = table.get_string(fields=fields)
+	except Exception,e:
+	    print "Unable to filter results. %s. Continuing to print the table." % e
     print table
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='float.py - Utility to identify information related to floating IPs')
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('--floatingip', dest='floatingip', type=str, help='Provides information related to provided floating IP', required=False, default=None)
+    group.add_argument('--floating-ip', dest='floating_ip', type=str, help='Provides information related to provided floating IP', required=False, default=None)
     group.add_argument('--network-id', dest='network_id', type=str, help='Provides floating IPs related to provided floating network ID', required=False, default=None)
-    group.add_argument('--fixedip', dest='fixedip', type=str, help='Provides floating IPs related to provided fixed ID. (Warning: May return multiple matches)', required=False, default=None)
-    group.add_argument('--tenant-id', dest='tenant_id', type=str, help='Provides floating IPs related to provided tenant ID.', required=False, default=None)
+    group.add_argument('--fixed-ip', dest='fixed_ip', type=str, help='Provides floating IPs related to provided fixed ID (Warning: May return multiple matches)', required=False, default=None)
+    group.add_argument('--tenant-id', dest='tenant_id', type=str, help='Provides floating IPs related to provided tenant ID', required=False, default=None)
+    parser.add_argument('-f', dest='fields', type=str, help='Limit output to specified fields using comma separated list', required=False, default=None)
+    parser.add_argument('-s', dest='sort', type=str, help='Sort by specified field', required=False, default=None)
 
     # Array for all arguments passed to script
     args = parser.parse_args()
